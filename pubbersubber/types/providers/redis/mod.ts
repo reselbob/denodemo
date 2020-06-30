4625e5e125b547d32aa8f13b4cb52caa8e6bbc36
Bob Reselman <reselbob@gmail.com>
2020-06-29 19:53:43 -0700

    fixing line car bug

--- pubbersubber/types/providers/redis/mod.ts
+++ pubbersubber/types/providers/redis/mod.ts
@@ -1,1 +1,5 @@
-export { Publisher } from './impl/publisher.ts';export { Subscriber } from './impl/subscriber.ts';export { PublisherConfigImpl } from './impl/publisher_config.ts';export { SubscriberConfigImpl } from './impl/subscriber_config.ts';export { IPublisherConfig } from './interfaces/publisher_config.ts';
+export { Publisher } from './impl/publisher.ts';
+export { Subscriber } from './impl/subscriber.ts';
+export { PublisherConfigImpl } from './impl/publisher_config.ts';
+export { SubscriberConfigImpl } from './impl/subscriber_config.ts';
+export { IPublisherConfig } from './interfaces/publisher_config.ts';
