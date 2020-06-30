249e24a2fd756d47f12cf54e179a488ba28d8e40
Bob Reselman <reselbob@gmail.com>
2020-06-29 19:42:57 -0700

    fixing line terminator bug

--- pubbersubber/types/base/pubbersubber.ts
+++ pubbersubber/types/base/pubbersubber.ts
@@ -1,1 +1,35 @@
-import {IPubberSubber} from '../interfaces/pubbersubber.ts';import {BackingService} from '../enums/backing_service.ts';import {IPubberSubberConfigBase} from '../interfaces/pubbersubber_config_base.ts';import {PubberSubberStatus} from '../enums/pubbersubbser_status.ts';import { v4 } from 'https://deno.land/std/uuid/mod.ts';export abstract class PubberSubberBase implements IPubberSubber {    backingService: BackingService    source: string;    id?: string;    routingKey?: string;    queueName?: string;    host?: string;    port?: number;    userName?: string;    password?: string;    connection: any    constructor(config: IPubberSubberConfigBase) {        this.id = v4.generate();        this.backingService = config.backingService        this.source = config.source;        this.routingKey = config.routingKey;        this.queueName = config.queueName;        this.host = config.host;        this.port = config.port;        this.userName = config.userName;        this.password = config.password;    }    async abstract connect(): Promise<PubberSubberStatus>;    async abstract disconnect(): Promise<PubberSubberStatus>;}
+import {IPubberSubber} from '../interfaces/pubbersubber.ts';
+import {BackingService} from '../enums/backing_service.ts';
+import {IPubberSubberConfigBase} from '../interfaces/pubbersubber_config_base.ts';
+import {PubberSubberStatus} from '../enums/pubbersubbser_status.ts';
+import { v4 } from 'https://deno.land/std/uuid/mod.ts';
+
+export abstract class PubberSubberBase implements IPubberSubber {
+    backingService: BackingService
+    source: string;
+    id?: string;
+    routingKey?: string;
+    queueName?: string;
+    host?: string;
+    port?: number;
+    userName?: string;
+    password?: string;
+    connection: any
+
+    constructor(config: IPubberSubberConfigBase) {
+        this.id = v4.generate();
+        this.backingService = config.backingService
+        this.source = config.source;
+        this.routingKey = config.routingKey;
+        this.queueName = config.queueName;
+        this.host = config.host;
+        this.port = config.port;
+        this.userName = config.userName;
+        this.password = config.password;
+    }
+
+    async abstract connect(): Promise<PubberSubberStatus>;
+
+    async abstract disconnect(): Promise<PubberSubberStatus>;
+
+}
--- pubbersubber/types/base/subscriber.ts
+++ pubbersubber/types/base/subscriber.ts
@@ -1,1 +1,7 @@
-import {ISubscriber} from "../interfaces/subscriber.ts";import {PubberSubberStatus} from "../enums/pubbersubbser_status.ts";import {PubberSubberBase} from "./pubbersubber.ts";export abstract class SubscriberBase extends PubberSubberBase implements ISubscriber {    async abstract subscribe(handler: Function): Promise<PubberSubberStatus>;}
+import {ISubscriber} from "../interfaces/subscriber.ts";
+import {PubberSubberStatus} from "../enums/pubbersubbser_status.ts";
+import {PubberSubberBase} from "./pubbersubber.ts";
+
+export abstract class SubscriberBase extends PubberSubberBase implements ISubscriber {
+    async abstract subscribe(handler: Function): Promise<PubberSubberStatus>;
+}
