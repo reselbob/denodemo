a1bacdb28288389bd72b42cf13ab73a8b584275c
Bob Reselman <reselbob@gmail.com>
2020-07-18 17:47:19 -0700

    fixing travis bug

--- fortune_cookies/test/export.bash
+++ fortune_cookies/test/export.bash
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd ..
-base=$PWD
-datadir=$base"/data"
-export DATA_DIR=$datadir
-echo "Here is: "$DATA_DIR
+export DATA_DIR=$PWD"/data/"
 #export DATA_DIR="$HOME/Documents/source-tree/denodemo/fortune_cookies/data/"
